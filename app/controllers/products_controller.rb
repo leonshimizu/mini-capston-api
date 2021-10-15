c3dbcaaf57986a323ba3570995bf5fcc400abb86
“leonshimizu” <lmshimizu@gmail.com>
2021-10-14 18:50:38 -0700

    products create - my attempt

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,9 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: id)
     render json: product.as_json
   end
+
+  def create
+    new_product = Product.new(name: Faker::Dessert.variety, price: Faker::Number.decimal(l_digits: 2, r_digits: 2))
+    render json: new_product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
  get "/products" => "products#index"
  get "/products/:id" => "products#show"
+ post "/products" => "products#create"
 end
