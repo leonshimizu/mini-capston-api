a305342eb12f1baf7931c20167f67a2f384dd859
“leonshimizu” <lmshimizu@gmail.com>
2021-10-19 18:26:15 -0700

    changed description from s to t

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_011943) do
+ActiveRecord::Schema.define(version: 2021_10_20_012327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_20_011943) do
     t.string "name"
     t.decimal "price", precision: 10, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
