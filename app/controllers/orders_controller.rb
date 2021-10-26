044b5cf7554980714a9ff3ae31491c556e7a61a2
“leonshimizu” <lmshimizu@gmail.com>
2021-10-26 16:30:42 -0700

    if user isn't logged in, they can't create, show, update, or index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,54 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find(params[:product_id])
+    if current_user
+      product = Product.find(params[:product_id])
 
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: product.id,
-      quantity: params[:quantity],
-      subtotal: product.price,
-      tax: product.tax,
-      total: product.total
-    )
-    order.save
-    render json: order
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: product.id,
+        quantity: params[:quantity],
+        subtotal: product.price,
+        tax: product.tax,
+        total: product.total
+      )
+      order.save
+      render json: order
+    else
+      render json: {message: "Sorry you must be logged in..."}
+    end
   end
 
   def show
-    order = Order.find(params[:id])
-    if order.user_id == current_user.id
-      render json: order
+    if current_user
+      order = Order.find(params[:id])
+      if order.user_id == current_user.id
+        render json: order
+      else
+        render json: {message: "Sorry, no order found..."}
+      end
     else
-      render json: {message: "Sorry, no order found..."}
+      render json: {message: "Sorry you must be logged in..."}
     end
   end
 
   def update
-    order = Order.find(params[:id])
-    order.product_id = params[:product_id] || order.product_id
-    order.quantity = params[:quantity] || order.quantity
-    order.save
-    render json: order
+    if current_user
+      order = Order.find(params[:id])
+      order.product_id = params[:product_id] || order.product_id
+      order.quantity = params[:quantity] || order.quantity
+      order.save
+      render json: order
+    else
+      render json: {message: "Sorry you must be logged in..."}
+    end
   end
 
   def index
-    orders = current_user.orders 
-    render json: orders
+    if current_user
+      orders = current_user.orders 
+      render json: orders
+    else
+      render json: {message: "Sorry you must be logged in..."}
+    end
   end
 end
