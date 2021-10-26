ccaaeeb1be9171c251b89349d8df2253746005f8
“leonshimizu” <lmshimizu@gmail.com>
2021-10-26 16:40:31 -0700

    added has_many/belongs_to to order and product serializers

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,4 +2,5 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :is_discounted?, :price, :tax, :total, :description, :how_many
   belongs_to :supplier
   has_many :images
+  has_many :orders
 end
