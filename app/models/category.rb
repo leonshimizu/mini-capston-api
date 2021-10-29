7cc63dc9874f744e73c8bc980da84164b49f7fc3
“leonshimizu” <lmshimizu@gmail.com>
2021-10-28 17:39:17 -0700

    added products method for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ApplicationRecord
   has_many :category_products
+
+  def products
+    products = []
+    i = 0
+    category_products.length.times do
+      product = Product.find(category_products[i].product_id)
+      products << product
+      i += 1
+    end
+    return products
+  end
 end
