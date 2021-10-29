e3852eb87576ce25de144d23dca159c0a53ccc25
“leonshimizu” <lmshimizu@gmail.com>
2021-10-28 18:50:47 -0700

    index filtered by category or shows all

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,14 @@ class ProductsController < ApplicationController
   before_action :authenticate_admin, only: [:create, :update, :destroy]
 
   def index
-    category = Category.find_by(name: params[:category])
-    products = category.products
-    render json: products
+    if params[:category]
+      category = Category.find_by(name: params[:category])
+      products = category.products
+      render json: products
+    else
+      products = Product.all
+      render json: products
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-20.times do    
-  product = Product.new(name: Faker::Device.model_name, price: rand(1..100), supplier_id: rand(1..5))
-  product.save
+# 20.times do    
+#   product = Product.new(name: Faker::Device.model_name, price: rand(1..100), supplier_id: rand(1..5))
+#   product.save
 
-  user = User.new(name: Faker::Name.first_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", admin: false)
-  user.save
+#   user = User.new(name: Faker::Name.first_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", admin: false)
+#   user.save
 
-  # image = Image.new()
-  # image.save
+#   # image = Image.new()
+#   # image.save
 
-  # order = Order.new()
-  # order.save
+#   # order = Order.new()
+#   # order.save
 
-  # supplier = Supplier.new()
-  # supplier.save
+#   # supplier = Supplier.new()
+#   # supplier.save
 
-  # category = Category.new()
-  # category.save
+#   # category = Category.new()
+#   # category.save
 
-  # cp = CategoryProduct.new()
-  # cp.save
-end
+#   # cp = CategoryProduct.new()
+#   # cp.save
+# end
