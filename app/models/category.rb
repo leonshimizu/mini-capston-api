af35fa407044181adf5fd3c58866d94a8aa24062
“leonshimizu” <lmshimizu@gmail.com>
2021-10-28 17:46:46 -0700

    added product and category methods for category and products (simple and complex methods) - many to many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,15 @@
 class Category < ApplicationRecord
   has_many :category_products
+  has_many :products, through: :category_products # same as code on line 5
 
-  def products
-    products = []
-    i = 0
-    category_products.length.times do
-      product = Product.find(category_products[i].product_id)
-      products << product
-      i += 1
-    end
-    return products
-  end
+  # def products
+  #   products = []
+  #   i = 0
+  #   category_products.length.times do
+  #     product = Product.find(category_products[i].product_id)
+  #     products << product
+  #     i += 1
+  #   end
+  #   return products
+  # end
 end
