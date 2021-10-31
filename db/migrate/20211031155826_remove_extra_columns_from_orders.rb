6baff945b352672bcd02056b3dfd1a5ff0ac6f5c
“leonshimizu” <lmshimizu@gmail.com>
2021-10-31 09:07:54 -0700

    created carted_products table and deleted extra columns from orders table

---
+++ db/migrate/20211031155826_remove_extra_columns_from_orders.rb
@@ -0,0 +1,7 @@
+class RemoveExtraColumnsFromOrders < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :orders, :user_id, :integer
+    remove_column :orders, :product_id, :integer
+    remove_column :orders, :quantity, :integer
+  end
+end
