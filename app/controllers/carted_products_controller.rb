959c435029d683bade11561b841783b6779a5659
“leonshimizu” <lmshimizu@gmail.com>
2021-11-01 17:35:13 -0700

    added filter for carted products index - user only sees their own orders

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -11,7 +11,7 @@ class CartedProductsController < ApplicationController
   end
 
   def index
-    carted_products = CartedProduct.where(status: "carted")
+    carted_products = current_user.carted_products.where(status: "carted")
     render json: carted_products.as_json
   end
 
