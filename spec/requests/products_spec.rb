89aa416634ed61b01fd7f723760dc25d8e1f1872
“leonshimizu” <lmshimizu@gmail.com>
2021-11-30 19:19:49 -0800

    more testing of rails rspec - everytime rpec is ran, it clears the previous db and gives you an empty one to work with

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -13,16 +13,18 @@ RSpec.describe "Products", type: :request do
   describe "GET /products" do 
     it "should return all products in the db" do 
       supplier = Supplier.create!(
-          name: "Tester",
-          email: "test@gmail.com",
+          name: "Tester 2",
+          email: "test2@gmail.com",
           phone_number: "0000000000"
-        )
+      )
       product = Product.create!(
-        name: "test 2",
+        name: "test 3",
         price: 0.00,
-        description: "testing something else",
+        description: "checking something cool",
         supplier_id: supplier.id
       )
+      # p Supplier.all
+      # p Product.all
       get "/products"
       products = JSON.parse(response.body)
       expect(response).to have_http_status(200)
